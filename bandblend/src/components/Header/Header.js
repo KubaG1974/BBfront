7154f3986cb2adcb5e8a861959fe475840251233
KubaG1974 <kuba.gorzynski@gmail.com>
2024-10-09 10:57:39 +0200

    Added Header component

---
+++ bandblend/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome to <span>BAND BLENDER</span></h1>
+      <p>Music tastes better together</p>
+    </header>
+  );
+}
+
+export default Header;
