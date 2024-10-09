30d188c7980a1f23dac831ea3f87e4dd5f249251
KubaG1974 <kuba.gorzynski@gmail.com>
2024-10-09 12:38:14 +0200

    Updated App.js

--- bandblend/src/App.js
+++ bandblend/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 import Header from './components/Header/Header';
 import AuthButton from './components/AuthButton/AuthButton';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Settings from './components/Settings';
+import Home from './components/Home/Home';
+import Profile from './components/Profile/Profile';
+import Settings from './components/Settings/Settings';
 import './App.css';
 
 function App() {
