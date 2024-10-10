b22c0dc871ce831268279c943acb88d8a1212a6d
KubaG1974 <kuba.gorzynski@gmail.com>
2024-10-10 19:00:39 +0200

    Changed css and added RWD for sidebar

--- bandblend/src/components/Sidebar/Sidebar.js
+++ bandblend/src/components/Sidebar/Sidebar.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaUser, FaCog } from 'react-icons/fa'; 
+import { FaHome, FaUser, FaCog, FaBars } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="sidebar">
-      <ul>
+    <div className = "sidebar">
+      <div className = "hamburger" onClick = {toggleMenu}>
+        <FaBars />
+      </div>
+      <ul className = {isOpen ? 'menu open' : 'menu'}>
         <li>
-          <Link to="/home">
-            <FaHome /> <span className="sidebar-text">Home</span>
+          <Link to = "/home">
+            <FaHome /> <span className = "sidebar-text">Home</span>
           </Link>
         </li>
         <li>
-          <Link to="/profile">
-            <FaUser /> <span className="sidebar-text">Profile</span>
+          <Link to = "/profile">
+            <FaUser /> <span className = "sidebar-text">Profile</span>
           </Link>
         </li>
         <li>
-          <Link to="/settings">
-            <FaCog /> <span className="sidebar-text">Settings</span>
+          <Link to = "/settings">
+            <FaCog /> <span className = "sidebar-text">Settings</span>
           </Link>
         </li>
       </ul>
